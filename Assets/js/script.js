52d44f0315c1693903f19b635cad476069c6950f
mahekrajput <mahekrajput260@gmail.com>
2025-11-20 20:22:55 +0530

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,14 @@
-// -------- GEOLOCATION ----------
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   } else {
-    document.getElementById("geo").innerHTML =
-      "Geolocation is not supported.";
+    document.getElementById("geoResult").innerText =
+      "Your browser does not support location.";
   }
 }
 
 function showPosition(position) {
-  document.getElementById("geo").innerHTML =
+  document.getElementById("geoResult").innerHTML =
     "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
-}
-
-if (document.getElementById("geo")) {
-  getLocation();
-}
-
-
-// -------- IMAGE PREVIEW ----------
-function openPreview(src) {
-  document.getElementById("preview-img").src = src;
-  document.getElementById("preview-box").style.display = "block";
-}
-
-function closePreview() {
-  document.getElementById("preview-box").style.display = "none";
 }
